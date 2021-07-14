346cf65ea8923c285344955b179209d3dab2da33
Loïs Tilma <loistilma@gmail.com>
2021-07-14 18:16:28 +0200

    Update iteration, header, cards, footer

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -2,21 +2,25 @@ var express = require('express');
 var router = express.Router();
 const fs = require('fs');
 
-let rawdata = fs.readFileSync('./data/datatest.json');
+let rawdata = fs.readFileSync('./data/restaurants.json');
 let restaurants = JSON.parse(rawdata);
 
 /* GET restaurant page. */
 router.get('/', function(req, res, next) {
+    var restaurant;
     for (i in restaurants) {
-        var restaurant = {};
         restaurantUrl = '/' + restaurants[i].url.split('.')[0];
         //console.log(restaurantUrl);
+        //console.log(req.originalUrl);
         if (restaurantUrl === req.originalUrl) {
+            //console.log('ok')
             restaurant = restaurants[i];
         }
+        //console.log(restaurant)
     }
+    
     //console.log(req.originalUrl);
-    //console.log(restaurant)
+    //console.log(restaurant);
     res.render('restaurant', { title: 'ohmyfood', pageTitle: restaurant.title, restaurant: restaurant });
 });
 
