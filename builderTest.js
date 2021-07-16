4a07700db3e4b3164c8c30c2f497d513ef6320a5
Loïs Tilma <loistilma@gmail.com>
2021-07-16 14:04:06 +0200

    Update cards

--- builderTest.js
+++ builderTest.js
@@ -2,7 +2,13 @@ const fs = require('fs');
 const pug = require('pug');
 
 // Compile template.pug, and render a set of data
-var indexFile = pug.renderFile('./views/index.pug', { pretty: true });
+var indexFile;
+var tempIndex = pug.renderFile('./views/index.pug', { pretty: true, title: 'ohmyfood', pageTitle: 'ohmyfood' });
+indexFile = tempIndex.replace('href="/la-palette-du-gout"', 'href="/la-palette-du-gout.html"')
+.replace('href="/la-note-enchantee"', 'href="/la-note-enchantee.html"')
+.replace('href="/a-la-francaise"', 'href="/a-la-francaise.html"')
+.replace('href="/le-delice-des-sens"', 'href="/le-delice-des-sens.html"');
+//console.log(indexFile)
 fs.writeFileSync('static_project/index.html', indexFile);
 let rawdata = fs.readFileSync('./data/restaurants.json');
 let restaurants = JSON.parse(rawdata);
